c980ffa5dddf737de21020b48be26874330d4cb4
Рассказов Роман <kartohahap12@gmail.com>
2025-12-06 21:18:19 +0500

    dz

--- tasks/task6.py
+++ tasks/task6.py
@@ -3,7 +3,8 @@
 def solve():
 # Ниже пишите решение задачи
 
-   
+    a, b, c = map(int, input().split())
+    print(a*a + b*b == c*c or a*a + c*c == b*b or b*b + c*c == a*a)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
